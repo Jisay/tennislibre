59d5a600c3e9f5f47da40085797067d517096ff4
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2018-11-23 17:05:08 +0100

    initial commit

---
+++ tennislibre.constants.js
@@ -0,0 +1,12 @@
+global.CONSTANTS = {
+  urls: {
+    tennislibre: 'https://www.tennislibre.com',
+  },
+  scripts: {
+    login: '/tennis/front/dologin.php',
+    reserve: '/tennis/front/reserve/take_m_reservation.php',
+  },
+  cookie: {
+    domain: 'www.tennislibre.com',
+  }
+};
